f90f62cabd10a4e600e0da3a95ee3150c707c657
Brian Carnes <carnes.brian.a@gmail.com>
2022-08-18 11:31:11 -0500

    created the Contacts class

--- src/contacts/Contacts.java
+++ src/contacts/Contacts.java
@@ -1,4 +1,15 @@
 package contacts;
 
+import java.util.HashMap;
+import java.util.Map;
 public class Contacts {
+    private final Map<String, Integer> contact = new HashMap<>();
+
+    public void setContact (String item, int amount) {
+        contact.put(item, amount);
+    }
+    public Map<String, Integer> getItemAmount () {
+        return contact;
+    }
+
 }
